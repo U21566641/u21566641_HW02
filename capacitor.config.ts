1da04b6f2c2f4ec96b0c47808d5a8f177c73777f
U21566641 <u21566641@tuks.co.za>
2023-04-22 21:44:37 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'u21566641_HW02',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
